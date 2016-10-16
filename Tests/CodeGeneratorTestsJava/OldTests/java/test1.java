c74740d6a3c33de6fcb1a9b7b54a520eb3b88dbe
Cabel Dhoj Shrestha <cabel.ds@gmail.com>
2016-10-16 12:03:10 -0500

    Cleaning out tests, docs and notes

--- Tests/CodeGeneratorTestsJava/OldTests/java/test1.java
+++
@@ -1,6 +0,0 @@
-public class test1 {
-    void foo() {
-	double d = 8.88;
-	int i = Integer.parseInteger(d);
-    }
-}
--- Tests/CodeGeneratorTestsJava/OldTests/java/test2.java
+++
@@ -1,6 +0,0 @@
-public class test2 {
-    public void foo() {
-	if (true)
-	    int x = 4;
-    }
-}
